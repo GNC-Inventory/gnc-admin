a9a3fd96f65a227aeca5ae1403912f65cf422e76
Samuel Edu <edubsam@gmail.com>
2025-09-16 16:25:26 +0100

    Updated Inventory Styling

--- src/app/inventory/page.tsx
+++ src/app/inventory/page.tsx
@@ -545,7 +545,6 @@ console.log('First filtered item:', filteredInventoryData[0]);
         <div className="text-sm font-medium text-gray-600 text-center">Description</div>
         <div className="text-sm font-medium text-gray-600 text-center">Stock left</div>
         <div className="text-sm font-medium text-gray-600 text-center">Unit cost</div>
-        <div className="text-sm font-medium text-gray-600 text-center">Profit %</div>
         <div className="text-sm font-medium text-gray-600 text-center">Amount</div>
         <div className="text-sm font-medium text-gray-600 text-center">Actions</div>
       </div>
